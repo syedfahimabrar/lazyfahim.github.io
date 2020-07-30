9986816fb18b115de794345512840cc84cb0adaf
fahim abrar <fahimabrar13@gmail.com>
2020-07-30 15:10:46 +0600

    medium blog added

--- js/main.js
+++ js/main.js
@@ -363,3 +363,99 @@ function LoadStory() {
     }
   });
 }
+
+$(function () {
+  var mediumPromise = new Promise(function (resolve) {
+  var $content = $('#jsonContent');
+  var data = {
+      rss: 'https://medium.com/feed/lazyfahim'
+  };
+  $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Flazyfahim', data, function (response) {
+      if (response.status == 'ok') {
+          var output = `<div class="blog-posts">`;
+          $.each(response.items, function (k, item) {
+              var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
+              var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
+              var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
+              var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
+              var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
+              // output += `<img src="${src}" class="card-img-top" alt="Cover image">`;
+              // output += `<div class="card-body">`;
+              // output += `<h5 class="card-title"><a href="${item.link}">${item.title}</a></h5>`;
+              var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
+              yourString = yourString.replace('h4', 'p');
+              yourString = yourString.replace('h3', 'p');
+              var maxLength = 120; // maximum number of characters to extract
+              //trim the string to the maximum length
+              var trimmedString = yourString.substr(0, maxLength);
+              //re-trim if we are in the middle of a word
+              trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
+              // output += `<p class="card-text">${trimmedString}...</p>`;
+              // output += `<a href="${item.link}" class="btn btn-outline-success">Read More</a>`;
+              // output += '</div></div>';
+              output += `<div class="blog-item">
+              <div class="blog-item-wrapper">
+                  <!-- blog item thumbnail -->
+                  <div class="blog-item-thumb">
+                      <a href="${item.link}"  target="_blank" class="loadPost"><img src="${src}" alt=""></a>
+                  </div>
+                  <!-- /blog item thumbnail -->
+                  <!-- Blog item - infos -->
+                  <div class="blog-item-infos">
+                      <!-- blog-item-title -->
+                      <div class="blog-item-title-wrapper">
+                          <h2 class="blog-item-title title-border"><a href="${item.link}"
+                          target="_blank"  class="loadPost">${item.title}</a></h2>
+                      </div>
+                      <!-- /blog-item-title -->
+                      <!-- blog item - description -->
+                      <div class="blog-item-description">
+                          <p><a href="${item.link}" target="_blank" class="loadPost">${trimmedString}....</a></p>
+                      </div>
+                      <!-- /blog-item-description -->
+                      <!-- blog item - link -->
+                      <div class="blog-item-link">
+                          <a href="${item.link}" target="_blank" class="btn btn-default loadPost">See More</a>
+                      </div>
+                      <!-- /blog item - link -->
+                  </div>
+                  <!-- /blog item - infos -->
+              </div>
+          </div>`;
+              return k < 10;
+          });
+          resolve($content.html(output));
+      }
+  });
+  });
+
+mediumPromise.then(function()
+  {
+      //Pagination
+      pageSize = 3;
+
+      var pageCount = $(".blog-item").length / pageSize;
+
+      for (var i = 0; i < pageCount; i++) {
+          $("#pagin").append(`<li class="page-item"><a class="page-link" href="#">${(i + 1)}</a></li> `);
+      }
+      $("#pagin li:nth-child(1)").addClass("active");
+      showPage = function (page) {
+          $(".blog-item").hide();
+          $(".blog-item").each(function (n) {
+              if (n >= pageSize * (page - 1) && n < pageSize * page)
+                  $(this).show();
+          });
+      }
+
+      showPage(1);
+
+      $("#pagin li").click(function () {
+          $("#pagin li").removeClass("active");
+          $(this).addClass("active");
+          showPage(parseInt($(this).text()))
+      });
+  });
+});
+
+
